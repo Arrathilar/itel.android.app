0efadbdf640116364ffb4d0c24ea0bca1adad916
Arrathilar <Wervvolf@gmail.com>
2019-01-12 18:53:42 +0200

    create login form

---
+++ app/src/main/java/ua/rv/itel/itel_serviceapp/HomeActivity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import ua.rv.itel.itel_serviceapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+    }
+}
